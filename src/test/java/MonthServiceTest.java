9e3e0bf0ecec3762893091cc5a7629229569a595
Irina <kalashiri-sweet@mail.ru>
2023-02-21 15:37:44 +0500

    Parameter Test

--- src/test/java/MonthServiceTest.java
+++ src/test/java/MonthServiceTest.java
@@ -1,33 +1,22 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class MonthServiceTest {
-    @Test
-    void shouldCalculateForMonth() {
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10000, 3000, 20000",
+            "2, 100000, 60000, 150000"
+    })
+    void shouldCalculateForMonth(long expected, int income, int expenses, int threshold) {
         MonthService service = new MonthService();
-        // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3000;
-        int threshold = 20_000;
-        long expected = 3;
-        // вызываем целевой метод:
-        long actual = service.calculate(income, expenses, threshold);
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-
-    }
-    @Test
-    void shouldCalculateForMonthSecond() {
-        MonthService service = new MonthService();
-        // подготавливаем данные:
-        int income = 100_000;
-        int expenses = 60000;
-        int threshold = 150_000;
-        long expected = 2;
-        // вызываем целевой метод:
         long actual = service.calculate(income, expenses, threshold);
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
 
     }
 }
+
+
+
