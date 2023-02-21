e45eb18ff422849b67a3bb00cdae79fa5746ffe8
Irina <kalashiri-sweet@mail.ru>
2023-02-21 19:37:13 +0500

    Delete main.java

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        MonthService service= new MonthService ();
-        int count = service.calculate(10000,3000,20000);
-        System.out.println(count);
-    }
-}
